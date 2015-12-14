25f467167e987a9f3454ee42c1d4f602f496634f
sllt <shenlongml@gmail.com>
2015-12-14 12:33:08 +0800

    update api

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -3,13 +3,21 @@ module V1
         resource :users do
             desc "return a user by id"
             params do
-                requires :id, type: Integer, desc: 'Status id.'
+                requires :id, type: Integer, desc: 'user id.'
             end
             route_param :id do
                 get do
                     User.find params[:id]
                 end
             end
+
+
+            desc "return user's topics"
+            params do
+                requires :id, type: Integer, desc: 'user id.'
+            end
+
+            get
         end
     end
 end
